c13e5023f6bb7f852ae723492f206aeea065a00e
John Zorko <jzorko@trulia.com>
2013-05-08 15:39:27 -0700

    initial rev

---
+++ Code/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Trulia
+//
+//  Created by Daniel Lowrie on 1/24/12.
+//  Copyright (c) 2012 Trulia Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
