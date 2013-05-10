71505ef1f95249644510426184af8dd4c5d75744
John Zorko <jzorko@trulia.com>
2013-05-10 15:52:31 -0700

    began work on filter view for iPhone

---
+++ Code/Iphone/ViewController/IRListingSearchViewControllerPhone.h
@@ -0,0 +1,13 @@
+//
+//  IRListingSearchViewControllerPhone.h
+//  IosRentalUniversal
+//
+//  Created by John Zorko on 5/10/13.
+//  Copyright (c) 2013 Trulia Inc. All rights reserved.
+//
+
+#import "ICListingSearchViewControllerDefault.h"
+
+@interface IRListingSearchViewControllerPhone : ICListingSearchViewControllerDefault
+
+@end
---
+++ Code/Iphone/ViewController/IRListingSearchViewControllerPhone.m
@@ -0,0 +1,38 @@
+//
+//  IRListingSearchViewControllerPhone.m
+//  IosRentalUniversal
+//
+//  Created by John Zorko on 5/10/13.
+//  Copyright (c) 2013 Trulia Inc. All rights reserved.
+//
+
+#import "IRListingSearchViewControllerPhone.h"
+
+@interface IRListingSearchViewControllerPhone ()
+
+@end
+
+@implementation IRListingSearchViewControllerPhone
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
