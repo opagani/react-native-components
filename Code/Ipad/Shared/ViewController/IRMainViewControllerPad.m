42de9a3f02fb9b447a6c0106b20e730f3fdaa510
Akshay Shah <ashah@trulia.com>
2013-05-13 15:47:56 -0700

    MOB-10940 - Override to remove Sort from iPad

--- Code/Ipad/Shared/ViewController/IRMainViewControllerPad.m
+++ Code/Ipad/Shared/ViewController/IRMainViewControllerPad.m
@@ -8,6 +8,9 @@
 
 #import "IRMainViewControllerPad.h"
 #import "IRFilterViewControllerPad.h"
+#import "IRListingSearchToolBarPad.h"
+
+#define TABLE_WIDTH_TOOLBAR 320
 
 @interface IRMainViewControllerPad ()
 
@@ -19,6 +22,11 @@
 
     if(!self.filterViewController)
 		self.filterViewController = [[IRFilterViewControllerPad alloc] init];
+}
+
+- (ICListingSearchToolBarPad *)searchToolBarPad{
+    
+    return [[IRListingSearchToolBarPad alloc] initWithFrame:CGRectMake(self.controlBarView.frame.size.width - TABLE_WIDTH_TOOLBAR, 0, TABLE_WIDTH_TOOLBAR, self.controlBarView.bounds.size.height) delegate:self];
 }
 
 @end
