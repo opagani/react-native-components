c355aaeeedadb42bfc15e3ac31aca9ea6b28722a
Jack Kwok <jkwok@trulia.com>
2013-05-14 17:41:34 -0700

    override heatmap configs

---
+++ Code/Iphone/ViewController/IRLayersMenuViewControllerPhone.m
@@ -0,0 +1,53 @@
+//
+//  IRLayersMenuViewControllerPhone.m
+//  IosRentalUniversal
+//
+//  Created by Jack Kwok on 5/14/13.
+//  Copyright (c) 2013 Trulia Inc. All rights reserved.
+//
+
+#import "IRLayersMenuViewControllerPhone.h"
+
+@interface IRLayersMenuViewControllerPhone ()
+
+@end
+
+@implementation IRLayersMenuViewControllerPhone
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (BOOL) isHeatmapSupported : (HEATMAP_TYPE) type {
+    switch (type) {
+        case HEATMAP_CRIME:
+            return YES;
+        case HEATMAP_FLOOD:
+            return YES;
+        case HEATMAP_SEISMIC:
+            return YES;
+        case HEATMAP_RENTAL_PRICES:
+            return YES;
+        default:
+            return NO;
+    }
+}
+
+@end
