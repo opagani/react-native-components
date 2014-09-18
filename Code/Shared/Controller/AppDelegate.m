e46359a732832e887bf2ecfd19074125054ed847
John Michael Zorko <johnz@gamespot.com>
2014-09-17 18:15:12 -0700

    fixed MovingCom show flag

--- Code/Shared/Controller/AppDelegate.m
+++ Code/Shared/Controller/AppDelegate.m
@@ -408,6 +408,8 @@ void uncaughtExceptionHandler(NSException *exception) {
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
+    [super applicationDidEnterBackground:application];
+    
     /*
      Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
      If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
