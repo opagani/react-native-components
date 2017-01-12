061463cf09780a7f85c150a6a633261cf84a534b
David Pan <dpan@dpan-trulia.lan>
2017-01-12 12:36:25 -0800

    MTI - migrating to new 3rd party sdk config pattern

---
+++ Trulia Rent/Classes/IRAdjustSDKConfig.h
@@ -0,0 +1,14 @@
+//
+//  IRAdjustSDKConfig.h
+//  Trulia Rent
+//
+//  Created by David Pan on 1/12/17.
+//  Copyright © 2017 Trulia Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ICAdjustSDKTrackingController.h"
+
+@interface IRAdjustSDKConfig : NSObject <ICAdjustSDKConfiguration>
+
+@end
---
+++ Trulia Rent/Classes/IRComscoreConfiguration.h
@@ -0,0 +1,13 @@
+//
+//  IRComscoreConfiguration.h
+//  Trulia Rent
+//
+//  Created by David Pan on 1/12/17.
+//  Copyright © 2017 Trulia Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ICComScore.h"
+@interface IRComscoreConfiguration : NSObject <ICComScoreConfiguration>
+
+@end
